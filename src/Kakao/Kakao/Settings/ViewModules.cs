2ce9d0ef0de59f442e071e52462e1bd48010d8eb
Brian Hwang <brianhwang29@gmail.com>
2024-04-09 10:32:12 +0900

    MainContent 추가

--- src/Kakao/Kakao/Settings/ViewModules.cs
+++ src/Kakao/Kakao/Settings/ViewModules.cs
@@ -2,6 +2,7 @@
 using Kakao.Core.Names;
 using Kakao.Friends.UI.Views;
 using Kakao.Login.UI.Views;
+using Kakao.Main.UI.Views;
 using Prism.Ioc;
 using Prism.Modularity;
 
@@ -18,6 +19,7 @@ namespace Kakao.Settings
         {
             //프리즘을 통해서 UI 객체들을 등록시켜 놓고 이제 폼쓰든 로그인이든 독립적으로, 참조를 걸지 않고도 다른 뷰를 가져와서 쓸 수 있게 해준다.
             containerRegistry.RegisterSingleton<IViewable, LoginContent>(ContentNameManager.Login);
+            containerRegistry.RegisterSingleton<IViewable, MainContent>(ContentNameManager.Main);
             containerRegistry.RegisterSingleton<IViewable, FriendsContent>(ContentNameManager.Friends);
         }
     }
