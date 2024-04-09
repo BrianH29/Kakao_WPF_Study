a95e71884ac5f25bd62d842f9e0c5ed302b7347a
Brian Hwang <brianhwang29@gmail.com>
2024-04-09 10:31:53 +0900

    Login 후 메인 구성

---
+++ src/Kakao/Kakao.Main/UI/Views/MainContent.cs
@@ -0,0 +1,19 @@
+﻿using Jamesnet.Wpf.Controls;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Kakao.Main.UI.Views
+{
+    public class MainContent : JamesContent
+    {
+        static MainContent()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MainContent), new FrameworkPropertyMetadata(typeof(MainContent)));
+        }
+
+        public MainContent()
+        {
+
+        }
+    }
+}
